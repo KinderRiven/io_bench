d49e8e90a672e87851211d45a685526d9e4ac437
K <1339764596@qq.com>
2020-11-08 20:54:30 +0800

    push by hsk

--- io_bench/file/readwrite.cc
+++ io_bench/file/readwrite.cc
@@ -118,7 +118,7 @@ int main(int argc, char** argv)
 
     time_t _t = time(NULL);
     struct tm* _lt = localtime(&_t);
-    sprintf(g_result_save_path, "[rw][%d%d][%04d%02d%02d][%02d%02d%02d]", _rbs, _wbs, _lt->tm_year, _lt->tm_mon, _lt->tm_mday, _lt->tm_hour, _lt->tm_min, _lt->tm_sec);
+    sprintf(g_result_save_path, "rw_%d_%d_%04d%02d%02d_%02d%02d%02d", _rbs, _wbs, _lt->tm_year, _lt->tm_mon, _lt->tm_mday, _lt->tm_hour, _lt->tm_min, _lt->tm_sec);
     mkdir(g_result_save_path, 0666);
 
     // create file
