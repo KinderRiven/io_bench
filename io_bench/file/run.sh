a408d48d1e3b933b5eea41c9803f401e7bd8fd18
K <1339764596@qq.com>
2020-11-08 19:12:27 +0800

    push by hsk

--- io_bench/file/run.sh
+++ io_bench/file/run.sh
@@ -1,4 +1,4 @@
-./test /home/hanshukai/dir1 1740 1
-./test /home/hanshukai/dir1 1740 1
-./test /home/hanshukai/dir1 1740 1 4096 50
-./test /home/hanshukai/dir1 1740 1 4096 50
+./seqwrite /home/hanshukai/dir1 1740 1
+./seqwrite /home/hanshukai/dir1 1740 1
+./randwrite /home/hanshukai/dir1 1740 1 4096 50
+./randwrite /home/hanshukai/dir1 1740 1 4096 50
