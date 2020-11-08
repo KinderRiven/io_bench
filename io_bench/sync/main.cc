947e6327950649555d5553bfa63aad40a1aa3a06
K <1339764596@qq.com>
2020-11-08 15:03:01 +0800

    push by hsk

---
+++ io_bench/sync/main.cc
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    char _dname[128];
+    size_t _size;
+    char* _base_address;
+
+    int _fd = open(_dname, O_RDWR | O_DIRECT, 0666);
+    assert(_fd > 0);
+
+    _base_address = (char*)mmap(nullptr, _size, PROT_READ | PROT_WRITE, MAP_SHARED, _fd, 0);
+    assert(_base_address != nullptr);
+    return 0;
+}
