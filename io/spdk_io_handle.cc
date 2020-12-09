a01df2d6ff57a85686554f81f661ef768ea4a862
K <1339764596@qq.com>
2020-12-09 22:18:48 +0800

    push by hsk

--- io/spdk_io_handle.cc
+++ io/spdk_io_handle.cc
@@ -226,7 +226,7 @@ do_seq_write: // 顺序写开始
         // 保存结果
         for (int j = 0; j < _io_depth; j++) {
             uint64_t _t = _io_ctx[j]->timer.Get();
-            io_thread->vec_latency.push_back(_t);
+            // io_thread->vec_latency.push_back(_t);
             // io_thread->total_time += _t;
         }
         // 判断结束方式
