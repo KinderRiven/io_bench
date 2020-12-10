11e7d56864141c426e5badf2291543c6ef7c264d
K <1339764596@qq.com>
2020-12-10 21:55:11 +0800

    push by hsk

--- io/spdk_io_handle.cc
+++ io/spdk_io_handle.cc
@@ -329,8 +329,9 @@ end:
     }
     io_thread->avg_time = 1.0 * io_thread->total_time / _do_count;
     io_thread->iops = 1000000000.0 / io_thread->avg_time;
-    printf("[thread:%02d][count:%llu][run_time:%.2fseconds][total_time:%lluns/%.2fseconds][avg_time:%.2fus][iops:%.2f]\n",
-        io_thread->thread_id, _do_count, _total_run_time, io_thread->total_time, 1.0 * io_thread->total_time / 1000000000UL, io_thread->avg_time / 1000, io_thread->iops);
+    double _bandwidth = (_io_total_size / (1024 * 1024)) / _total_run_time;
+    printf("[thread:%02d][count:%llu][bw:%.2fMB/s][run_time:%.2fseconds][total_time:%.2fseconds][avg_time:%.2fus][iops:%.2f]\n",
+        io_thread->thread_id, _do_count, _bandwidth, _total_run_time, 1.0 * io_thread->total_time / 1000000000UL, io_thread->avg_time / 1000, io_thread->iops);
     return;
 }
 
