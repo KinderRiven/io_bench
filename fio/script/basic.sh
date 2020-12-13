e4b67d9844bb44787a089d46f0d766d7fadc6bfa
K <1339764596@qq.com>
2020-12-13 16:04:22 +0800

    push by hsk

--- fio/script/basic.sh
+++ fio/script/basic.sh
@@ -1,20 +1,23 @@
 rw=('write' 'randwrite' 'read' 'randread')
 num_rw=${#rw[@]}
 
-bs=('512B' '1KB' '4KB' '16KB' '64KB')
+# bs=('512B' '1KB' '4KB' '16KB' '64KB')
+bs=('4KB' '16KB')
 num_bs=${#bs[@]}
+
+iodepth=(1 2 4 8 16 32)
+num_iodepth=${#iodepth[@]}
+
+numjobs=(1 2 4 8)
+num_numjobs=${#numjobs[@]}
 
 directory='/home/hanshukai/mount/4800'
 
 ioengine=libaio 
 
-iodepth=8
-
 runtime=3600
 
 log_avg_msec=5000
-
-numjobs=4
 
 random_distribution=random
 
@@ -32,14 +35,22 @@ _bs=${bs[$i]}
 for ((j=0; j<${num_rw}; j+=1))
 do
 _rw=${rw[$j]}
-_dir=$time/${_bs}_${_rw}_${iodepth}_${numjobs}
+for ((k=0; k<${num_numjobs}; k++))
+do
+_numjobs=${numjobs[$k]}
+for ((l=0; l<${num_iodepth}; l++))
+do
+_iodepth=${iodepth[$l]}
+_dir=$time/${_bs}_${_rw}_${_numjobs}_${_iodepth}
 echo $_dir
 mkdir $_dir
 _log=${_dir}/log
-_shell="-name=wjob -rw=${_rw} -bs=${_bs} -fallocate=1 -thread -directory=${directory} -ioengine=${ioengine} -iodepth=${iodepth} -time_based --runtime=${runtime} -log_avg_msec=${log_avg_msec}\
- -write_bw_log=${_log} -write_iops_log=${_log} -write_lat_log=${_log} -numjobs=${numjobs} \
+_shell="-name=wjob -rw=${_rw} -bs=${_bs} -fallocate=1 -thread -directory=${directory} -ioengine=${ioengine} -iodepth=${_iodepth} -time_based --runtime=${runtime} -log_avg_msec=${log_avg_msec}\
+ -write_bw_log=${_log} -write_iops_log=${_log} -write_lat_log=${_log} -numjobs=${_numjobs} \
  -random_distribution=${random_distribution} -filesize=${filesize} -nrfiles=${nrfiles} --output=${_dir}/result.txt"
 echo $_shell
 fio $_shell
+done
+done
 done
 done
