cfa92a390f6d4c154671075a7824b5bad5f8491f
K <1339764596@qq.com>
2020-12-30 19:56:50 +0800

    push by hsk

--- demo/mmap.cc
+++ demo/mmap.cc
@@ -42,8 +42,10 @@ int main(int argc, char** argv)
     Timer _timer;
     int _scan;
     char* _name = argv[1];
-    size_t _size = atol(argv[2]);
+    size_t _size = atol(argv[2]) * 1024 * 1024;
     size_t _block_size = atol(argv[3]);
+
+    printf("[%s][size:%zu][bs:%zu]\n", _name, _size, _block_size);
 
     int _fd = open(_name, O_RDWR | O_DIRECT, 0666);
     void* _base = mmap(nullptr, _size, PROT_READ | PROT_WRITE, MAP_SHARED, _fd, 0);
